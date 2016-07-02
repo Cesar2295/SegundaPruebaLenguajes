593d819001bf487b501f52602bf89bc56d9529d5
Cesar Rivra <cesar_cukii22@hotmail.com>
2016-07-02 13:28:08 -0600

    Nuevas variables en ModeloCustomParaLaVista

--- Notificador/Notificador.UI/Models/ModeloParaLaVistaConfiguracionDeNotificacion.cs
+++ Notificador/Notificador.UI/Models/ModeloParaLaVistaConfiguracionDeNotificacion.cs
@@ -7,5 +7,15 @@ namespace Notificador.UI.Models
 {
     public class ModeloParaLaVistaConfiguracionDeNotificacion
     {
+
+
+        public int Id { get; set; }
+
+        public int Dia { get; set; }
+
+        public int HoraNotificacion { get; set; }
+
+
+
     }
 }
